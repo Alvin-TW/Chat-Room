7440d587b636ff36da9fad40a99a146b6b251771
Alvin Tsai <bestkun0401@gmail.com>
2017-10-04 16:27:21 +0800

    Modify actionCreators.js unsing ES6

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,31 +1,8 @@
-export const setUserInfo = userinfo => {
-	return { type: 'SET_USERINFO', userinfo: userinfo }
-}
-
-export const setUserId = uid => {
-	return { type: 'SET_USERID', uid: uid }
-}
-
-export const updateUserList = userlist => {
-	return { type: 'UPDATE_USERLIST', userlist: userlist }
-}
-
-export const updateMessages = messages => {
-	return { type: 'UPDATE_MESSAGE', messages }
-}
-
-export const clearMessages = () => {
-	return { type: 'CLEAR_MESSAGE' }
-}
-
-export const leaveChatRoom = () => {
-	return { type: 'LEAVE_CHATROOM' }
-}
-
-export const changeMessageBoxColor = color => {
-	return { type: 'CHANGE_MESSAGEBOXCOLOR', color: color }
-}
-
-export const setErrorInfo = error => {
-	return { type: 'SET_ERRORINFO', error: error }
-}
+export const setUserInfo = userinfo => ({ type: 'SET_USERINFO', userinfo: userinfo })
+export const setUserId = uid => ({ type: 'SET_USERID', uid })
+export const updateUserList = userlist => ({ type: 'UPDATE_USERLIST', userlist })
+export const updateMessages = messages => ({ type: 'UPDATE_MESSAGE', messages })
+export const clearMessages = () => ({ type: 'CLEAR_MESSAGE' })
+export const leaveChatRoom = () => ({ type: 'LEAVE_CHATROOM' })
+export const changeMessageBoxColor = color => ({ type: 'CHANGE_MESSAGEBOXCOLOR', color })
+export const setErrorInfo = error => ({ type: 'SET_ERRORINFO', error })
